59861a5b131fb0d1665f9aa01c61bae65440877e
sophiehelf <sophie.helf@gmail.com>
2017-11-27 12:11:20 -0500

    post request working

--- src/NewDream.js
+++ src/NewDream.js
@@ -1,18 +1,56 @@
 import React from 'react'
 
 class NewDream extends React.Component {
-	constructor() {
-		super()
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			content: "",
+			name: ""
+		}
+	}
+
+	handleNameChange = (e) => {
+		e.preventDefault();
+		this.setState({
+			name: e.target.value
+		})
+		console.log(this.state.name)
+	}
+
+	handleContentChange = (e) => {
+		e.preventDefault()
+		this.setState({
+			content: e.target.value
+		})
+		console.log(this.state.content)
+	}
+
+	makePostRequest = (e) => {
+		fetch("http://localhost:3001/dreams", {
+			method: "post",
+			headers: {
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: this.state.name,
+				content: this.state.content
+			})
+		}).then((res) => {
+			console.log(res)
+		})
 	}
 
 	render() {
 		return(
 			<div className="new-dream">
 			<h3>New dream</h3>
-				<textarea placeholder="Describe your dream here..." name="dream" className="dream-text-input">
+				<textarea placeholder="Name your dream" name="dream-name" onChange={this.handleNameChange} className="dream-name-input"></textarea> <br /> <br />
+				<textarea placeholder="Describe your dream here..." name="dream" className="dream-text-input" onChange={this.handleContentChange}>
 				</textarea>
 				<br />
-				<input type="submit" value="Send it off" />
+				<input type="submit" value="Send it off" onClick={this.makePostRequest}/>
 			</div>
 		)
 	}
